292457637a65166a705574a169b1c3fe7bb14f9f
tinasheben101 <tinasheben101@gmail.com>
2022-12-11 11:02:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *   *
  *    * Return: Always 0 (Success)
  *     */
-int main(void)
+int main(void)	
 {
+
 	char str1[] = "\"Programming is like building a multilingual puzzle";
 	puts(str1);
-	
 	return 0;
 
 }
