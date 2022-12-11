4ebbb71e90c386846c83a9ef13ff54aed32cf7b1
tinasheben101 <tinasheben101@gmail.com>
2022-12-11 15:53:23 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,11 +25,10 @@ int main(void)
 	}
 	if (x == 0)
 	{                                                                                                                                                                                     
-        printf("Last digit of %d is %d", n, x);                                                                                                                                     
-        }
+        printf("Last digit of %d is %d and is 0\n", n, x);                                                                                                                                            }
         if (x < 6 && x != 0)
 	{
-	printf("Last digit of %d is %d", n, x); 
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x); 
 	}
 	return (0);
 }
