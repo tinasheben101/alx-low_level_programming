23a04d51a06bb93a8efcc8fba84601953b74066f
tinasheben101 <tinasheben101@gmail.com>
2022-12-11 18:58:29 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+# include <stdio.h> 
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description' 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 0; x  < 10, x++)
+putchar(n);
+putchar('\n');
+return (0);
+}
