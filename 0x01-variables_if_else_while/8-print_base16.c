fdc10f8923c770fe65ff4f9f3f69543a12282544
tinasheben101 <tinasheben101@gmail.com>
2022-12-11 21:18:14 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+int num;
+char letter;
+
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
