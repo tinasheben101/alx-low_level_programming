593483744032dd270545455d96f40fb77264d6d2
tinasheben101 <tinasheben101@gmail.com>
2022-12-11 16:19:26 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Description: 'the program's description'
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for(int x = 'A'; x <= 'Z'; x++)
+	putchar(tolower(x));
+}
