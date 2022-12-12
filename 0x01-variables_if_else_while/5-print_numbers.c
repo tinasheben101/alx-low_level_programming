3b8d0aa2a6124ec0727fd23e3eeda3512d89e4c9
tinasheben101 <tinasheben101@gmail.com>
2022-12-11 18:21:58 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+ 
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+printf("%d", x)
+return (0);
+}
+
+
