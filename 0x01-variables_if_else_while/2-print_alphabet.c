7be1e761e77bcb4cacc343b6ba4e3908260da83c
tinasheben101 <tinasheben101@gmail.com>
2022-12-11 16:25:31 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
 # include <stdio.h>
 /**
- *  main - Entry point
+ * main - Entry point
  *
- *  Description: 'the program's description'
+ * Description: 'the program's description'
  *
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
