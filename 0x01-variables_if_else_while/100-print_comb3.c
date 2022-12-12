dc3929fc1303d7fde54c6129d4efe3482ff78721
tinasheben101 <tinasheben101@gmail.com>
2022-12-12 11:41:08 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,8 +17,9 @@ for (num1 = 0; num1 <= 98; num1++)
 	{
 	putchar((num1 / 10) + '0');
 	putchar((num1 % 10) + '0');
+	putchar(' ');
 	putchar((num2 / 10) + '0');
-	putchar((num2 % 10) + '0');
+	putchar((num2 % 10) + '0' + '\n');
 
 	if (num1 == 98 && num2 == 99)
 		continue;
@@ -27,7 +28,6 @@ for (num1 = 0; num1 <= 98; num1++)
 }
 }
 
-putchar('\n');
 
 return (0);
 }
