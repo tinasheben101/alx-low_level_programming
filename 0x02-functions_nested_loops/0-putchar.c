15417436a5ce5471229f0c229e0f21d7874a0e7a
tinasheben101 <tinasheben101@gmail.com>
2022-12-13 20:54:47 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,16 @@
  *
  * Returns Always 0 (Success)
  */
-
 int main(void)
-{ 
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_pitchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+  	_putchar('\n');
+	return (0);
 }
