5b2e836b434e6f05e82ea2907c2ef7381845c469
tinasheben101 <tinasheben101@gmail.com>
2022-12-13 17:59:22 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ * Description: 'the program's description'
+ *
+ * Returns Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
