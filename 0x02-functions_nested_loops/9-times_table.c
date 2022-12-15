d8f874e4081983d77bb11065f63a74caafe36c16
tinasheben101 <tinasheben101@gmail.com>
2022-12-14 18:34:01 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,28 +10,30 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		n = i * j;
-
-		if ((n / 10) == 0)
+		for (j = 0; j <= 9; j++)
 		{
-			if (j != 0)
+			n = i * j;
+
+		        if ((n / 10) == 0)
+	        	{ 
+				if (j != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+
+				if (j == 9)
+
+					continue;
+				_putchar(',');
 				_putchar(' ');
-			_putchar(n + '0');
-
-			if (j == 9)
-
-				continue;
-			_putchar(',');
-			_putchar(' ');
 		}
-		else
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			if (j == 9)
-				continue;
-			_putchar(',');
-			_putchar(' ');
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
 		}
 	}
 	_putchar('\n');
