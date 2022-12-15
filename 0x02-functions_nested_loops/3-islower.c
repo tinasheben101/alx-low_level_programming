74f32007106ea67bfaffa4a8af089105a9c9492f
tinasheben101 <tinasheben101@gmail.com>
2022-12-14 16:51:57 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+# include "main.h"
+/**
+ * main - Entry point
+ * Description: 'function that checks for lowercase character.'
+ * 
+ * Returns Always 0 (Success)
+ */
+int main(void)
+{
+int r;
+if (c >= 'a' && c <= 'z')
+	return (1);
+else
+	return (0);
+}
+
