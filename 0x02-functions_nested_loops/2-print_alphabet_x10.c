4daafefbbc8ad8af17c153805df43e7b6e76b4d2
tinasheben101 <tinasheben101@gmail.com>
2022-12-14 16:32:01 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ * main - Entry point
+ * Description: 'the program's description' 
+ *
+ *  Returns Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+			_putchar('\n');
+	}
+}
