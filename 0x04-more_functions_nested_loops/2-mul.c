0c9421996f0002e322d2ae6528e511cb1c665d4c
tinasheben101 <tinasheben101@gmail.com>
2022-12-15 18:53:43 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,18 +1,12 @@
 #include "main.h"
+
 /**
- * print_line - a function that draws a straight line in the terminal.
- * followed by a new line.
- * @n: An input integer
+ * mul - a function that multiplies two integers.
+ * @a: An input integer
+ * @b: An input integer
  * Return: Always 0
  */
-void print_line(int n)
+int mul(int a, int b)
 {
-	int i = 0;
-
-	if (n > 0)
-	{
-		for (; i < n; i++)
-			_putchar('_');
-	}
-	_putchar('\n');
+	return (a * b);
 }
