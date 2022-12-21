9fd7a6644e85880a9228a6f493fe35934a3d24a4
tinasheben101 <tinasheben101@gmail.com>
2022-12-20 17:50:04 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _puts - Write a function that prints a string, followed
+ * by a new line, to stdout.
+ * @str: an input string
+ * Return: Nothing
  */
-int main(void)
+void _puts(char *str)
 {
-	char *str;
+	int i = 0;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_puts(str);
-	return (0);
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
