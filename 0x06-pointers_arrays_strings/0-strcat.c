dfea4bba8b8f864611e118e8e9ccdbc758560419
tinasheben101 <tinasheben101@gmail.com>
2022-12-22 16:02:41 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,20 @@
 #include "main.h"
 /**
- *
+ *main - check the code
+ *Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char dest = "Hello";
+char src = "World";
+char *ptr;
+
+printf("%s\n", dest);
+printf("%s", src);
+ptr = _strcat(dest, src);
+printf("%s", dest);
+printf("%s", src);
+printf("%s", ptr);
+return dest;
+}
