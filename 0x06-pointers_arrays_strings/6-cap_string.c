e407d9f3337862d69feb1a28435332f942a51f87
tinasheben101 <tinasheben101@gmail.com>
2022-12-22 17:17:44 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int check_seperators(char c);
 /**
  * cap_string - a function that capitalizes all words of a string.
  * @s: An input string to capitalize letters
@@ -21,3 +22,21 @@ char *cap_string(char *s)
 	return (s);
 }
 
+/**
+ * check_seperators - Separators of words: space, tabulation, new line,
+ * ,, ;, ., !, ?, ", (, ), {, and }
+ * @c: an input character
+ * Return: 1 if seperator, 0 otherwise
+ */
+int check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
+}
