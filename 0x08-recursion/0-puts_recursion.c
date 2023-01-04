80f8a86dcd57a12f6c81ebea9f6667b6b92326fb
tinasheben101 <tinasheben101@gmail.com>
2023-01-03 19:54:45 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * program to prints a string, followed by a new line.
+ * character string
+ */
+void _puts_recursion(char *s)
+{
+	puts(*s);
+	puts('/n');
+
+	return 0;
+}
